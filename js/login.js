3da4410fe9232b28b27e87b3273e000954703b3c
VivekPandey07 <vivek.pandey@hnrtech.com>
2026-03-20 21:06:39 +0530

    testing

--- js/login.js
+++ js/login.js
@@ -2,7 +2,6 @@
 	'use strict'
 
 	// Fetch all the forms we want to apply custom Bootstrap validation styles to
-	var forms = document.querySelectorAll('.needs-validation')
 
 	// Loop over them and prevent submission
 	Array.prototype.slice.call(forms)
